31c050c530f8a03dec46aaa181a1c95d507fddcd
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-05-10 21:01:48 +0100

    Delete Producto1_FP066_PRUEBA-main - copia/frontend directory

--- Producto1_FP066_PRUEBA-main - copia/frontend/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PORT: 3000,
-    GRAPHQL_PATH: '/graphql',
-  };
-  
--- Producto1_FP066_PRUEBA-main - copia/frontend/config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://mvidalda:Ytj5FLg1ruJeM0t6@clusterfp066.30mncau.mongodb.net/calendar?retryWrites=true&w=majority',
-    MONGODB_OPTIONS: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  };
-  
